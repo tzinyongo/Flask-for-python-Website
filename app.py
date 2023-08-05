9f245b29c5d5ff3cfcf91a37e4b56064cf8df3ec
Oliver Samuels <osamuels@bu.edu>
2023-08-05 18:44:03 -0400

    change in third_page route on app.py and change fetch name from /search to /thirdpage

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
 from flask import Flask, render_template, request, jsonify
 import requests
 app = Flask(__name__)
-app.jinja_env.auto_reload = True  # This enables auto-reloading of templates during development.
+app.jinja_env.auto_reload = True
 app.config['TEMPLATES_AUTO_RELOAD'] = True
-# Your view functions and other configurations go here.
+
 
 RAPIDAPI_KEY = "c31808e9camsh88eaebb5c2c6f84p158cf2jsne30fc5cd6235"
 @app.route('/')
@@ -14,28 +14,26 @@ def index():
 def second_page():
     return render_template('secondpage.html')
 
-@app.route('/thirdpage')
+@app.route('/thirdpage', methods = ['GET','POST'])
 def third_page():
+    if request.method == 'POST':
+        query = request.form['query']
+        url = "https://yahoo-finance127.p.rapidapi.com/search/"+query
+        headers = {
+            "X-RapidAPI-Key": "a7794b9a36mshed157cdb4537f8ap1b3af5jsn3878cc4f1a3c",
+            "X-RapidAPI-Host": "yahoo-finance127.p.rapidapi.com"
+        }
+        response = requests.get(url, headers=headers)
+
+        if response.status_code == 200:
+            data = response.json()
+            results = data.get('quotes', [])
+            print(results)
+            return jsonify(results)
+        else:
+            return jsonify([])
+
     return render_template('thirdpage.html')
-
-@app.route('/thirdpage', methods=['POST'])
-def search():
-    query = request.form['query']
-
-    headers = {
-        'X-RAPIDAPI-KEY': RAPIDAPI_KEY
-    }
-
-    api_url = "https://apidojo-yahoo-finance-v1.p.rapidapi.com/auto-complete"
-    response = requests.get(api_url, headers=headers)
-
-    if response.status_code == 200:
-        data = response.json()
-        results = data['results']
-        return jsonify(results)
-    else:
-        return jsonify([])
-
     #return render_template('thirdpage.html')
 
 
