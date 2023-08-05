42e5300ddb69607047614679e210400b6a69217e
tzinyongo <tadzinyongo@gmail.com>
2023-08-05 18:08:58 -0400

    implementing api

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
-from flask import Flask, render_template
-
+from flask import Flask, render_template, request, jsonify
+import requests
 app = Flask(__name__)
 app.jinja_env.auto_reload = True  # This enables auto-reloading of templates during development.
 app.config['TEMPLATES_AUTO_RELOAD'] = True
 # Your view functions and other configurations go here.
 
+RAPIDAPI_KEY = "c31808e9camsh88eaebb5c2c6f84p158cf2jsne30fc5cd6235"
 @app.route('/')
 def index():
     return render_template('index.html')
@@ -12,6 +13,32 @@ def index():
 @app.route('/secondpage')
 def second_page():
     return render_template('secondpage.html')
+
+@app.route('/thirdpage')
+def third_page():
+    return render_template('thirdpage.html')
+
+@app.route('/thirdpage', methods=['POST'])
+def search():
+    query = request.form['query']
+
+    headers = {
+        'X-RAPIDAPI-KEY': RAPIDAPI_KEY
+    }
+
+    api_url = "https://apidojo-yahoo-finance-v1.p.rapidapi.com/auto-complete"
+    response = requests.get(api_url, headers=headers)
+
+    if response.status_code == 200:
+        data = response.json()
+        results = data['results']
+        return jsonify(results)
+    else:
+        return jsonify([])
+
+    #return render_template('thirdpage.html')
+
+
 
 if __name__ == '__main__':
     app.run()
--- static/js/main1.js
+++ static/js/main1.js
@@ -5,3 +5,48 @@ function myFunction(){
 function anotherfunction(){
     window.location.href = "/";
 }
+function thirdpage()
+{
+    window.location.href = "/thirdpage";
+}
+
+function search(){
+    var query = document.getElementById('query').value;
+    console.log('Query value:', query);
+    fetch('/search',  {
+    method: 'POST', 
+    headers:     {
+        'Content-Type': 'application/x-www-form-urlencoded'
+    },
+    body: 'query=' + encodeURIComponent(query)
+
+    })
+
+    .then(response=> response.json())
+    .then(data => {
+        var resultsDiv = document.getElementById('search-results');
+        resultsDiv.innerHTML = '<h3>Search Results:</h3>';
+        if(data.length > 0) 
+        {
+            for(var i = 0; i < data.length; i++)
+            {
+                resultsDiv.innerHTML += '<p>' + data[i] + '</p>';
+
+            }
+        }
+        else {
+                resultsDiv.innerHTML += '<p>No results found.</p>'; 
+            }
+        
+    })
+    .catch(error => {
+        console.error('Error Fetching search results:', error); 
+    }
+        )
+
+
+}
+
+    
+    
+    
